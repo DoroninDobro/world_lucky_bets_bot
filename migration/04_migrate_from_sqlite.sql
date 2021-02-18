99c871509a32e7d25a43abd86b3ff146c022f239
Yuriy Chebyshev <szQ.qnsy39HJG,!>
2021-02-19 00:51:04 +0300

    fix database format

--- migration/04_migrate_from_sqlite.sql
+++ migration/04_migrate_from_sqlite.sql
@@ -20,6 +20,8 @@ ALTER TABLE bookmakers ALTER COLUMN created TYPE timestamp with time zone;
 ALTER TABLE bets_log ALTER COLUMN created TYPE timestamp with time zone;
 ALTER TABLE work_threads ALTER COLUMN start TYPE timestamp with time zone;
 
+# next commands only for missing sequences
 CREATE SEQUENCE send_to_workers_id_seq MINVALUE 1000 START 1000;
 ALTER TABLE send_to_workers ALTER id SET DEFAULT nextval('send_to_workers_id_seq');
+ALTER SEQUENCE send_to_workers_id_seq OWNER TO fortunate_bets_bot;
 ALTER SEQUENCE send_to_workers_id_seq OWNED BY send_to_workers.id;
