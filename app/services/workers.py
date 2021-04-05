e47bcf52893c1a63fc78cc501a3f153263266464
Yuriy Chebyshev <ychebyshev@it.ru>
2021-04-05 22:15:26 +0300

    fix not wanted=)

--- app/services/workers.py
+++ app/services/workers.py
@@ -16,7 +16,7 @@ async def add_worker_to_thread(user: User, message_id: int, bot: Bot, *, thread:
         config.USER_LOG_CHAT_ID,
         f"{datetime.now(tz=config.tz_view)} - {user.mention_link} присоединился к работе над матчем {thread.id}"
     )
-    texts = await thread.additional_texts.filter(is_draft=False, is_disinformation=False).order_by("id").all()
+    texts = await thread.additional_texts.filter(is_draft=False, is_disinformation=False).all()
     if texts:
         send_text = OLD_MESSAGE_SEPARATOR.join([text.text for text in texts])
         await bot.send_message(chat_id=user.id, text=send_text, reply_to_message_id=message_id)
