2459cea19673c6958d431e608c2eabed1fce6ac0
Yuriy Chebyshev <ychebyshev@it.ru>
2021-12-03 22:59:46 +0300

    remove hints

--- app/services/rates/openexchangerates.py
+++ app/services/rates/openexchangerates.py
@@ -1,58 +1,1 @@
-from datetime import datetime, date
-from decimal import Decimal
-
-from openexchangerates.client import OpenExchangeRatesClient
-
-from app.services.rates.rates import Rates
-
-
-class OpenExchangeRates(Rates):
-
-    def __init__(self, api_key: str):
-        super(OpenExchangeRates, self).__init__()
-        self.api_key = api_key
-        self.r = OpenExchangeRatesClient(api_key=self.api_key)
-
-    async def get_updated_date(self) -> datetime:
-        latest_res = await self.r.latest()
-        return datetime.fromtimestamp(latest_res['timestamp'])
-
-    async def _convert(self, code_from: str, code_to: str, value: Decimal = Decimal(1)) -> Decimal:
-        if code_from == code_to:
-            return value
-        else:
-            return value * await self.get_rate(code_from, code_to)
-
-    async def get_rate(self, code_to: str, code_from: str) -> Decimal:
-        if code_from == code_to:
-            return Decimal(1)
-        else:
-            rates = (await self.r.latest())['rates']
-            return rates[code_to] / rates[code_from]
-
-    async def convert(
-            self, code_from: str, code_to: str, value: Decimal = Decimal(1), day: date = None
-    ) -> Decimal:
-        if day is None:
-            return await self._convert(code_from, code_to, value)
-        if code_from == code_to:
-            return value
-        else:
-            return value * await self.get_rate_historical(code_from, code_to, day)
-
-    async def get_rate_historical(self, code_to: str, code_from: str, day: date) -> Decimal:
-        if code_from == code_to:
-            return Decimal(1)
-        else:
-            rates = (await self.r.historical(day))['rates']
-            return rates[code_to] / rates[code_from]
-
-    @property
-    def source_rates(self):
-        return 'OpenExchangeRates'
-
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.r.close()
+from datetime import datetimefrom decimal import Decimalfrom openexchangerates.client import OpenExchangeRatesClientfrom app.services.rates.rates import Ratesclass OpenExchangeRates(Rates):    def __init__(self, api_key):        super(OpenExchangeRates, self).__init__()        self.api_key = api_key        self.r = OpenExchangeRatesClient(api_key=self.api_key)    async def get_updated_date(self):        latest_res = await self.r.latest()        return datetime.fromtimestamp(latest_res['timestamp'])    async def _convert(self, code_from, code_to, value=Decimal(1)):        if code_from == code_to:            return value        else:            return value * await self.get_rate(code_from, code_to)    async def get_rate(self, code_to, code_from):        if code_from == code_to:            return Decimal(1)        else:            rates = (await self.r.latest())['rates']            return rates[code_to] / rates[code_from]    async def convert(self, code_from, code_to, value=Decimal(1), day=None):        if day is None:            return await self._convert(code_from, code_to, value)        if code_from == code_to:            return value        else:            return value * await self.get_rate_historical(code_from, code_to, day)    async def get_rate_historical(self, code_to, code_from, day):        if code_from == code_to:            return Decimal(1)        else:            rates = (await self.r.historical(day))['rates']            return rates[code_to] / rates[code_from]    @property    def source_rates(self):        return 'OpenExchangeRates'    async def __aenter__(self):        return self    async def __aexit__(self, exc_type, exc_val, exc_tb):        await self.r.close()
--- app/services/rates/utils.py
+++ app/services/rates/utils.py
@@ -1,22 +1,1 @@
-from datetime import date
-from decimal import Decimal
-
-from app.models import RateItem
-from app.services.rates import OpenExchangeRates
-from app.config import BASE_CURRENCY
-
-
-async def find_rate_and_convert(
-        value: Decimal,
-        currency: str,
-        day: date,
-        oer: OpenExchangeRates,
-        rates: dict[date: list[RateItem]]
-) -> Decimal:
-    try:
-        rate: RateItem = rates[day][currency]
-    except KeyError:
-        result = await oer.convert(currency, BASE_CURRENCY, value, day)
-    else:
-        result = rate.convert_internal(BASE_CURRENCY, value)
-    return result
+from app.config import BASE_CURRENCYasync def find_rate_and_convert(value, currency, day, oer, rates):    try:        rate = rates[day][currency]    except KeyError:        result = await oer.convert(currency, BASE_CURRENCY, value, day)    else:        result = rate.convert_internal(BASE_CURRENCY, value)    return result
