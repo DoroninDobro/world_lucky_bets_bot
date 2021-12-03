9a3f9ecfc62764b03221a645f074e9831cfa0a2d
Yuriy Chebyshev <ychebyshev@it.ru>
2021-12-03 23:50:14 +0300

    fixes

--- app/services/reports/common.py
+++ app/services/reports/common.py
@@ -5,7 +5,12 @@ async def get_mont_bets(date_range):
     return await BetItem \
         .filter(worker_thread__work_thread__start__gte=date_range.start) \
         .filter(worker_thread__work_thread__start__lt=date_range.stop) \
-        .prefetch_related("bookmaker", "worker_thread", "worker_thread__work_thread", "worker_thread__worker").all()
+        .prefetch_related(
+            "bookmaker",
+            "worker_thread",
+            "worker_thread__work_thread",
+            "worker_thread__worker"
+        ).all()
 
 
 async def get_month_rates(date_range):
