9f29d0f4ab72cb68bb4b8a1a872e80bad390492f
Yuriy Chebyshev <ychebyshev@it.ru>
2021-12-03 22:15:44 +0300

    remove hints

--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -1,7 +1,1 @@
-from aiogram import Dispatcher
-
-from app.middlewares.acl import ACLMiddleware
-
-
-def setup(dispatcher: Dispatcher):
-    dispatcher.middleware.setup(ACLMiddleware())
+from app.middlewares.acl import ACLMiddlewaredef setup(dispatcher):    dispatcher.middleware.setup(ACLMiddleware())
--- app/middlewares/acl.py
+++ app/middlewares/acl.py
@@ -1,34 +1,1 @@
-# partially from https://github.com/aiogram/bot
-
-from typing import Optional
-
-from aiogram import types
-from aiogram.dispatcher.middlewares import BaseMiddleware
-
-from app.models.db.chat import Chat
-from app.models.db.user import User
-from app.utils.lock_factory import LockFactory
-
-
-class ACLMiddleware(BaseMiddleware):
-    def __init__(self):
-        super(ACLMiddleware, self).__init__()
-        self.lock_factory = LockFactory()
-
-    async def setup_chat(self, data: dict, user: types.User, chat: Optional[types.Chat] = None):
-        try:
-            async with self.lock_factory.get_lock(f"{user.id}"):
-                user = await User.get_or_create_from_tg_user(user)
-            async with self.lock_factory.get_lock(f"{chat.id}"):
-                chat = await Chat.get_or_create_from_tg_chat(chat)
-
-        except Exception as e:
-            raise e
-        data["user"] = user
-        data["chat"] = chat
-
-    async def on_pre_process_message(self, message: types.Message, data: dict):
-        await self.setup_chat(data, message.from_user, message.chat)
-
-    async def on_pre_process_callback_query(self, query: types.CallbackQuery, data: dict):
-        await self.setup_chat(data, query.from_user, query.message.chat if query.message else None)
+# partially from https://github.com/aiogram/botfrom aiogram.dispatcher.middlewares import BaseMiddlewarefrom app.models.db.chat import Chatfrom app.models.db.user import Userfrom app.utils.lock_factory import LockFactoryclass ACLMiddleware(BaseMiddleware):    def __init__(self):        super(ACLMiddleware, self).__init__()        self.lock_factory = LockFactory()    async def setup_chat(self, data, user, chat=None):        try:            async with self.lock_factory.get_lock(f"{user.id}"):                user = await User.get_or_create_from_tg_user(user)            async with self.lock_factory.get_lock(f"{chat.id}"):                chat = await Chat.get_or_create_from_tg_chat(chat)        except Exception as e:            raise e        data["user"] = user        data["chat"] = chat    async def on_pre_process_message(self, message, data):        await self.setup_chat(data, message.from_user, message.chat)    async def on_pre_process_callback_query(self, query, data):        await self.setup_chat(data, query.from_user, query.message.chat if query.message else None)
