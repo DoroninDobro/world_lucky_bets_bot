798bf349643ec912acc02eb3dc6c2001bf882999
Yuriy Chebyshev <ychebyshev@it.ru>
2021-12-10 19:18:40 +0300

    copy messages, not forward

--- app/handlers/channels.py
+++ app/handlers/channels.py
@@ -7,5 +7,8 @@ from app.misc import dp
 
 @dp.channel_post_handler(chat_id=config.WORKERS_CHAT_ID)
 async def forwarding_all(message: types.Message):
-    await message.forward(config.ADMINS_WITHOUT_USERNAMES_LOG_CHAT_ID)
+    await message.bot.send_message(
+        config.ADMINS_WITHOUT_USERNAMES_LOG_CHAT_ID,
+        message.html_text
+    )
     raise SkipHandler
