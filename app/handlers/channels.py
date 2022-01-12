7e3c94eba16f82e86ed08aa3bf91e5c36064b215
Yuriy Chebyshev <ychebyshev@it.ru>
2022-01-12 23:12:24 +0300

    add remove username for channel forwarding

--- app/handlers/channels.py
+++ app/handlers/channels.py
@@ -3,12 +3,13 @@ from aiogram.dispatcher.handler import SkipHandler
 
 from app import config
 from app.misc import dp
+from app.utils.text_utils import remove_usernames
 
 
 @dp.channel_post_handler(chat_id=config.WORKERS_CHAT_ID)
 async def forwarding_all(message: types.Message):
     await message.bot.send_message(
         config.ADMINS_WITHOUT_USERNAMES_LOG_CHAT_ID,
-        message.html_text
+        remove_usernames(message.html_text),
     )
     raise SkipHandler
