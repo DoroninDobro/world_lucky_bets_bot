358b5faf290c8ff48b865245b8b660e015a742c6
bomzheg <bomzheg@gmail.com>
2022-04-26 17:16:50 +0300

    rename middleware

--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -5,14 +5,14 @@ from aiogram import Dispatcher
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from loguru import logger
 
-from app.middlewares.acl import ACLMiddleware
+from app.middlewares.db_middleware import DbMiddleware
 from app.middlewares.access_control import AccessControlMiddleware
 from app import config
 
 
 def setup(dispatcher: Dispatcher):
     logger.info("Configure middlewares...")
-    dispatcher.middleware.setup(ACLMiddleware())
+    dispatcher.middleware.setup(DbMiddleware())
     if config.ENABLE_ALLOW_LIST:
         with open(config.allow_list_path) as f:
             allow_list = set(json.load(f))
