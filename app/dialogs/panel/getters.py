0701653a148ac9adc3db944e58a94ec2c2f280cc
Yuriy Chebyshev <ychebyshev@it.ru>
2022-04-28 22:31:04 +0300

    balance for admin

--- app/dialogs/panel/getters.py
+++ app/dialogs/panel/getters.py
@@ -3,6 +3,8 @@ from typing import Any
 from aiogram_dialog import DialogManager
 
 from app.models import User
+from app.models.config import Config
+from app.rendering.balance import render_balance
 from app.services.balance import calculate_balance
 from app.services.rates import OpenExchangeRates
 from app.services.workers import get_registered
@@ -16,11 +18,14 @@ async def get_users(**kwargs):
 
 async def get_user(dialog_manager: DialogManager, **kwargs):
     data: dict[str, Any] = dialog_manager.current_context().dialog_data
-    config = dialog_manager.data["config"]
+    config: Config = dialog_manager.data["config"]
     user = await User.get(id=data["active_user"])
     async with OpenExchangeRates(api_key=config.currencies.oer_api_token) as oer:
         return {
             "user": user,
-            "balance": await calculate_balance(user, oer=oer, config=config.currencies),
+            "balance": render_balance(
+                balance=await calculate_balance(user, oer=oer, config=config.currencies),
+                currency=config.currencies.default_currency,
+            ),
         }
 
