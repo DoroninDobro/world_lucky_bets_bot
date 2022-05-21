ea5f876d06c8653c1a06760d640fc2d56f702672
Yuriy Chebyshev <ychebyshev@it.ru>
2022-05-21 23:52:17 +0300

    optimize requests rate

--- app/services/reports/excel_report.py
+++ app/services/reports/excel_report.py
@@ -5,6 +5,8 @@ from aiogram.types import InputFile
 from app.models import DataTimeRange
 from app.models.config import Config
 from app.services.excel_writter import ExcelWriter
+from app.services.rates import OpenExchangeRates
+from app.services.rates.converter import RateConverter
 from app.services.reports.thread_users import generate_thread_users
 from app.services.reports.total_report import generate_total_report
 from app.services.reports.user_stat import generate_user_report
@@ -13,9 +15,11 @@ from app.services.reports.user_stat import generate_user_report
 async def process_report(date_range: DataTimeRange, config: Config) -> InputFile:
     config.temp_path.mkdir(exist_ok=True, parents=True)
     excel_writer = ExcelWriter(config.currencies)
-    excel_writer.insert_total_report(await generate_total_report(date_range, config.currencies))
-    excel_writer.insert_thread_users(await generate_thread_users(date_range))
-    excel_writer.insert_users_reports(await generate_user_report(date_range, config.currencies))
+    async with OpenExchangeRates(config.currencies.oer_api_token) as oer:
+        converter = RateConverter(oer=oer, date_range=date_range)
+        excel_writer.insert_total_report(await generate_total_report(date_range, converter, config.currencies))
+        excel_writer.insert_thread_users(await generate_thread_users(date_range))
+        excel_writer.insert_users_reports(await generate_user_report(date_range, converter, config.currencies))
     filename = config.temp_path / f"rez{date_range}___{time()}.xlsx"
     excel_writer.save(filename)
     return InputFile(filename, f"{date_range}.xlsx")
--- app/services/reports/total_report.py
+++ app/services/reports/total_report.py
@@ -1,32 +1,33 @@
 from app.models import TotalStatistic, DataTimeRange
 from app.models.config.currency import CurrenciesConfig
 from app.models.db import WorkThread
-from app.services.rates import OpenExchangeRates
 from app.services.rates.converter import RateConverter
 from app.services.reports.common import get_mont_bets
 
 
-async def generate_total_report(date_range: DataTimeRange, config: CurrenciesConfig) -> dict[int, TotalStatistic]:
+async def generate_total_report(
+        date_range: DataTimeRange,
+        converter: RateConverter,
+        config: CurrenciesConfig,
+) -> dict[int, TotalStatistic]:
     bets_log = await get_mont_bets(date_range)
     total_statistics = {}
-    async with OpenExchangeRates(config.oer_api_token) as oer:
-        converter = RateConverter(oer=oer, date_range=date_range)
-        for bet_item in bets_log:
-            thread: WorkThread = bet_item.worker_thread.work_thread
-            day = thread.start.date()
-            search_kwargs = dict(
-                currency=bet_item.currency, day=day, currency_to=config.default_currency.iso_code,
-            )
-            bet = await converter.find_rate_and_convert(value=bet_item.bet, **search_kwargs)
-            result = await converter.find_rate_and_convert(value=bet_item.result, **search_kwargs)
-            total_statistic = TotalStatistic(
-                day=day,
-                thread=thread,
-                total_bet_eur=bet,
-                total_result_eur=result - bet,
-                total_payment_eur=result,
-            )
-            append_in_statistic_in_dict(total_statistics, thread.id, total_statistic)
+    for bet_item in bets_log:
+        thread: WorkThread = bet_item.worker_thread.work_thread
+        day = thread.start.date()
+        search_kwargs = dict(
+            currency=bet_item.currency, day=day, currency_to=config.default_currency.iso_code,
+        )
+        bet = await converter.find_rate_and_convert(value=bet_item.bet, **search_kwargs)
+        result = await converter.find_rate_and_convert(value=bet_item.result, **search_kwargs)
+        total_statistic = TotalStatistic(
+            day=day,
+            thread=thread,
+            total_bet_eur=bet,
+            total_result_eur=result - bet,
+            total_payment_eur=result,
+        )
+        append_in_statistic_in_dict(total_statistics, thread.id, total_statistic)
     return total_statistics
 
 
